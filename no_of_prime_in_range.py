1417b9495fc131f52c11fc65a65cee93fab85998
Kaviprakash156 <36467048+Kaviprakash156@users.noreply.github.com>
2018-05-24 12:03:58 +0530

    Update and rename prime_in_range.py to no_of_prime_in_range.py

--- no_of_prime_in_range.py
+++ no_of_prime_in_range.py
@@ -1,9 +1,12 @@
 a=int(input())
 b=int(input())
+counting=0
 for c in range(a,b+1):
    if c > 1:
        for i in range(2,c):
            if (c % i) == 0:
                break
        else:
-           print(c)
+           pass
+           counting+=1
+print (counting)
